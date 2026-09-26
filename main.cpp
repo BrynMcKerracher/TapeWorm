4a70635e9eec82f94724d54c8f6f8131f8b5ba2c
float <floatunderflow@proton.me>
2026-09-26 12:35:59 +1200

    Expand no file error message

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@
 #include "AST/Builder.h"
 #include "JIT/Compiler.h"
 
-#include <chrono>
 #include <iostream>
 
 int main(const int argc, char** argv) {
     //Check correct number of arguments
     if (argc < 2) {
-        std::cerr << "Error: No file given to interpret.\n";
-        std::cerr << "Example Usage: './TapeWorm <filename>\n";
+        std::cerr << "Error: No source file given.\n";
+        std::cerr << "Please make sure to reference a brainfuck source file.\n";
+        std::cerr << "Example Usage: './TapeWorm filename.b'\n";
         return 0;
     }
 
@@ -26,7 +26,7 @@ int main(const int argc, char** argv) {
 
     const std::string fileString = TapeWorm::Util::BrainFuckFileToString(argv[1]);
     const auto tokens = scanner.Scan(fileString);
-    auto ast = treeBuilder.BuildAST(tokens);
+    const auto ast = treeBuilder.BuildAST(tokens);
     const auto ops = compilerVisitor.Visit(ast);
 
     jitCompiler.Compile(ops);
